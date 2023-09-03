d580bbdb27c2e9e95afb6b3a541ec405134789da
Nay Khant Kyaw <naykhantkyaw@acedatasystems.com>
2023-09-03 16:17:38 +0630

    [Complete] Add user with Ajax

---
+++ EmployeeManagement/Controllers/AddUserController.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AddUserController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        string connection;
+
+        public AddUserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connection = _configuration.GetConnectionString("myConnection");
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(EmployeeUsers users)
+        {
+            if (!ModelState.IsValid) return BadRequest("Enter Requrired Fields");
+            SqlConnection con = new SqlConnection(connection);
+            con.Open();
+            string query = "Insert into EmployeeUser Values('" + users.UserName + "','" + users.Password + "')";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            return this.Ok("Form User Received");
+
+        }
+    }
+}
--- EmployeeManagement/Models/EmployeeUsers.cs
+++ EmployeeManagement/Models/EmployeeUsers.cs
@@ -1,9 +1,13 @@
-﻿namespace EmployeeManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
 {
     public class EmployeeUsers
     {
         public int Id { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
